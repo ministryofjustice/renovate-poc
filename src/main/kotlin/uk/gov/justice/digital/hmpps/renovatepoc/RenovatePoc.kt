e8706237f62c01e9d4c63290738ac5e77d72de2c
neilmendum <neilmendum@users.noreply.github.com>
2022-04-05 14:22:40 +0000

    updating project name after deployment from template repository

--- src/main/kotlin/uk/gov/justice/digital/hmpps/renovatepoc/RenovatePoc.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/renovatepoc/RenovatePoc.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.hmppstemplatepackagename
+package uk.gov.justice.digital.hmpps.renovatepoc
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication()
-class HmppsTemplateKotlin
+class RenovatePoc
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<RenovatePoc>(*args)
 }
